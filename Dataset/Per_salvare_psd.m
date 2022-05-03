e60a52ad4bce749205f4e17d3dfd642b1ae0fef9
Erica Espinosa <erica.espinosa.f@gmail.com>
2022-05-03 22:13:33 +0200

    power specral density data by channel and component

---
+++ Dataset/Per_salvare_psd.m
@@ -0,0 +1,107 @@
+%% Gruppo ADHD
+
+for ch = 1:19
+    
+    % comp 1
+    filename = append('ADHD_Matrici_wavelettati/ch',string(ch),'_c1.mat');
+    mat = load(filename).c1;
+
+    p = pwelch(mat',[],[],[],128);
+    p = 10*log10(p');
+
+    save(append('ADHD_Matrici_psd/ch',string(ch),'_p1'),'p')
+    
+    % comp 2
+    filename = append('ADHD_Matrici_wavelettati/ch',string(ch),'_c2.mat');
+    mat = load(filename).c2;
+
+    p = pwelch(mat',[],[],[],128);
+    p = 10*log10(p');
+
+    save(append('ADHD_Matrici_psd/ch',string(ch),'_p2'),'p')
+    
+    % comp 3
+    filename = append('ADHD_Matrici_wavelettati/ch',string(ch),'_c3.mat');
+    mat = load(filename).c3;
+
+    p = pwelch(mat',[],[],[],128);
+    p = 10*log10(p');
+
+    save(append('ADHD_Matrici_psd/ch',string(ch),'_p3'),'p')
+    
+    
+    % comp 4
+    filename = append('ADHD_Matrici_wavelettati/ch',string(ch),'_c4.mat');
+    mat = load(filename).c4;
+
+    p = pwelch(mat',[],[],[],128);
+    p = 10*log10(p');
+
+    save(append('ADHD_Matrici_psd/ch',string(ch),'_p4'),'p')
+    
+    
+    % comp 5
+    filename = append('ADHD_Matrici_wavelettati/ch',string(ch),'_c5.mat');
+    mat = load(filename).c5;
+
+    p = pwelch(mat',[],[],[],128);
+    p = 10*log10(p');
+
+    save(append('ADHD_Matrici_psd/ch',string(ch),'_p5'),'p')
+    
+end
+
+%% Gruppo di controllo
+
+for ch = 1:19
+    
+    % comp 1
+    filename = append('Control_Matrici_wavelettati/ch',string(ch),'_c1.mat');
+    mat = load(filename).c1;
+
+    p = pwelch(mat',[],[],[],128);
+    p = 10*log10(p');
+
+    save(append('Control_Matrici_psd/ch',string(ch),'_p1'),'p')
+    
+    % comp 2
+    filename = append('Control_Matrici_wavelettati/ch',string(ch),'_c2.mat');
+    mat = load(filename).c2;
+
+    p = pwelch(mat',[],[],[],128);
+    p = 10*log10(p');
+
+    save(append('Control_Matrici_psd/ch',string(ch),'_p2'),'p')
+    
+    % comp 3
+    filename = append('Control_Matrici_wavelettati/ch',string(ch),'_c3.mat');
+    mat = load(filename).c3;
+
+    p = pwelch(mat',[],[],[],128);
+    p = 10*log10(p');
+
+    save(append('Control_Matrici_psd/ch',string(ch),'_p3'),'p')
+    
+    
+    % comp 4
+    filename = append('Control_Matrici_wavelettati/ch',string(ch),'_c4.mat');
+    mat = load(filename).c4;
+
+    p = pwelch(mat',[],[],[],128);
+    p = 10*log10(p');
+
+    save(append('Control_Matrici_psd/ch',string(ch),'_p4'),'p')
+    
+    
+    % comp 5
+    filename = append('Control_Matrici_wavelettati/ch',string(ch),'_c5.mat');
+    mat = load(filename).c5;
+
+    p = pwelch(mat',[],[],[],128);
+    p = 10*log10(p');
+
+    save(append('Control_Matrici_psd/ch',string(ch),'_p5'),'p')
+    
+end
+
+
