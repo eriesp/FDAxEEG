200a72dc5998c471a56945c2a38be5bfe735da08
unknown <jclazza@gmail.com>
2022-05-07 16:10:03 +0200

    aggiunto matricic pyulear

--- Dataset/Approssimazioni_pyulear.m
+++ Dataset/Approssimazioni_pyulear.m
@@ -25,7 +25,7 @@ close
 Q_w_med = median(trapz(p_w));
 Q_w_sum = sum(trapz(p_w));
 
-ord_vec = 10:2:40;
+ord_vec = 20:2:50;
 
 n = length(ord_vec);
 
@@ -79,4 +79,55 @@ ord_vec(I)
 % [H,F] = freqz(1,mat(1:10)',[],1);
 % plot(F,20*log10(abs(H)))
 
+%% Campione misto tra ADHD e Control
 
+clear
+
+ch = 1;
+filename_control = append('Control_Matrici_wavelettati/ch',string(ch),'_c5.mat');
+mat_control = load(filename_control).c5;
+
+filename_adhd = append('ADHD_Matrici_wavelettati/ch',string(ch),'_c5.mat');
+mat_adhd = load(filename_adhd).c5;
+
+mat_misto = [mat_control(1:100,:);mat_adhd(1:100,:)];
+
+[p_w,f_w] = pwelch(mat_misto',[],[],[],128);
+
+Q_w_med = median(trapz(p_w));
+Q_w_sum = sum(trapz(p_w));
+
+ord_vec = 1:1:100;
+
+n = length(ord_vec);
+
+Q_med = zeros(n,1);
+Q_sum = zeros(n,1);
+
+ii = 1;
+
+for or = ord_vec
+    [p, f] = pyulear(mat_misto',or,[],128);
+    Q = trapz(p);
+    Q_med(ii) = median(Q);
+    Q_sum(ii) = sum(Q);
+    
+    ii = ii+1;
+end
+
+Q_dif_med = abs(Q_med - Q_w_med);
+Q_dif_sum = abs(Q_sum - Q_w_sum);
+
+subplot(1,2,1)
+plot(ord_vec,Q_dif_med)
+title('diff of median')
+
+subplot(1,2,2)
+plot(ord_vec,Q_dif_sum)
+title('diff of sum')
+
+[~,I] = min(Q_dif_med);
+ord_vec(I) 
+
+[p, f] = pyulear(mat_misto',35,[],128);
+plot(f(1:60),p(:,1:60))
--- Dataset/Per_salvare_pyulear.m
+++ Dataset/Per_salvare_pyulear.m
@@ -1,106 +1,110 @@
 %% Gruppo ADHD
 
+clear
+
 for ch = 1:19
     
     % comp 1
     filename = append('ADHD_Matrici_wavelettati/ch',string(ch),'_c1.mat');
     mat = load(filename).c1;
+    or=95;
+    [p, f] = pyulear(mat',or,[],128);
 
-    [p, f] = pwelch(mat',[],[],[],128);
-    p = 10*log10(p');
 
-    save(append('ADHD_Matrici_psd/ch',string(ch),'_p1'),'p','f')
+    save(append('ADHD_Matrici_pyulear/ch',string(ch),'_p1'),'p','f')
     
     % comp 2
     filename = append('ADHD_Matrici_wavelettati/ch',string(ch),'_c2.mat');
     mat = load(filename).c2;
+    or=89;
+    [p, f] = pyulear(mat',or,[],128);
 
-    [p, f] = pwelch(mat',[],[],[],128);
-    p = 10*log10(p');
 
-    save(append('ADHD_Matrici_psd/ch',string(ch),'_p2'),'p')
+    save(append('ADHD_Matrici_pyulear/ch',string(ch),'_p2'),'p','f')
     
     % comp 3
     filename = append('ADHD_Matrici_wavelettati/ch',string(ch),'_c3.mat');
     mat = load(filename).c3;
+    or=58;
+    [p, f] = pyulear(mat',or,[],128);
 
-    [p, f] = pwelch(mat',[],[],[],128);
-    p = 10*log10(p');
 
-    save(append('ADHD_Matrici_psd/ch',string(ch),'_p3'),'p')
+    save(append('ADHD_Matrici_pyulear/ch',string(ch),'_p3'),'p','f')
     
     
     % comp 4
     filename = append('ADHD_Matrici_wavelettati/ch',string(ch),'_c4.mat');
     mat = load(filename).c4;
+    or=97;
+    [p, f] = pyulear(mat',or,[],128);
 
-    [p, f] = pwelch(mat',[],[],[],128);
-    p = 10*log10(p');
 
-    save(append('ADHD_Matrici_psd/ch',string(ch),'_p4'),'p')
+    save(append('ADHD_Matrici_pyulear/ch',string(ch),'_p4'),'p','f')
     
     
     % comp 5
     filename = append('ADHD_Matrici_wavelettati/ch',string(ch),'_c5.mat');
     mat = load(filename).c5;
+    or=13;
+    [p, f] = pyulear(mat',or,[],128);
 
-    p = pwelch(mat',[],[],[],128);
-    p = 10*log10(p');
 
-    save(append('ADHD_Matrici_psd/ch',string(ch),'_p5'),'p')
+    save(append('ADHD_Matrici_pyulear/ch',string(ch),'_p5'),'p','f')
     
 end
 
 %% Gruppo di controllo
+
+clear
 
 for ch = 1:19
     
     % comp 1
     filename = append('Control_Matrici_wavelettati/ch',string(ch),'_c1.mat');
     mat = load(filename).c1;
+    or=95;
+    [p, f] = pyulear(mat',or,[],128);
 
-    p = pwelch(mat',[],[],[],128);
-    p = 10*log10(p');
 
-    save(append('Control_Matrici_psd/ch',string(ch),'_p1'),'p')
+    save(append('Control_Matrici_pyulear/ch',string(ch),'_p1'),'p','f')
     
     % comp 2
     filename = append('Control_Matrici_wavelettati/ch',string(ch),'_c2.mat');
     mat = load(filename).c2;
+    or=89;
+    [p, f] = pyulear(mat',or,[],128);
 
-    p = pwelch(mat',[],[],[],128);
-    p = 10*log10(p');
 
-    save(append('Control_Matrici_psd/ch',string(ch),'_p2'),'p')
+    save(append('Control_Matrici_pyulear/ch',string(ch),'_p2'),'p','f')
     
     % comp 3
     filename = append('Control_Matrici_wavelettati/ch',string(ch),'_c3.mat');
     mat = load(filename).c3;
+    or=58;
+    [p, f] = pyulear(mat',or,[],128);
 
-    p = pwelch(mat',[],[],[],128);
-    p = 10*log10(p');
 
-    save(append('Control_Matrici_psd/ch',string(ch),'_p3'),'p')
+    save(append('Control_Matrici_pyulear/ch',string(ch),'_p3'),'p','f')
     
     
     % comp 4
     filename = append('Control_Matrici_wavelettati/ch',string(ch),'_c4.mat');
     mat = load(filename).c4;
+    or=97;
+    [p, f] = pyulear(mat',or,[],128);
 
-    p = pwelch(mat',[],[],[],128);
-    p = 10*log10(p');
 
-    save(append('Control_Matrici_psd/ch',string(ch),'_p4'),'p')
+    save(append('Control_Matrici_pyulear/ch',string(ch),'_p4'),'p','f')
     
     
     % comp 5
     filename = append('Control_Matrici_wavelettati/ch',string(ch),'_c5.mat');
     mat = load(filename).c5;
+    or=13;
+    [p, f] = pyulear(mat',or,[],128);
 
-    p = pwelch(mat',[],[],[],128);
-    p = 10*log10(p');
 
-    save(append('Control_Matrici_psd/ch',string(ch),'_p5'),'p')
+    save(append('Control_Matrici_pyulear/ch',string(ch),'_p5'),'p','f')
     
 end
 
