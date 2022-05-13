e132522f0496dd643ed41f96493eaadb8f95a98f
unknown <jclazza@gmail.com>
2022-05-13 15:03:48 +0200

    aggiunto cartella media segnali

--- Dataset/Interpolazione.py
+++ Dataset/Interpolazione.py
@@ -52,43 +52,45 @@ from sklearn.cluster import AgglomerativeClustering
 # for i in np.arange(len(PSD_ADHD)):
 #     Fp_ADHD.append(np.mean([Prefrontal[0][i], Prefrontal[1][i]],axis=0))
 
-canale=input('Canale: ')
+#canale=input('Canale: ')
 banda=input('Banda: ')
+p=[]
+for i in np.arange(1,20):
+    canale=str(i)
+    filename_adhd = r"C:\Users\Asus\OneDrive\Desktop\NL2\FDAxEEG\Dataset\ADHD_Matrici_pyulear\ch"+canale+"_p"+banda
+    mat = scipy.io.loadmat(filename_adhd)
+    PSD_ADHD=mat['p'].T
+    df_Channel=pd.DataFrame(data=PSD_ADHD)
+    Channel=df_Channel.to_numpy(dtype=None, copy=False)
+    Channel = np.nan_to_num(Channel)
+    ADHD=skfda.FDataGrid(data_matrix=Channel)
+    ADHD.interpolation=skfda.representation.interpolation.SplineInterpolation(interpolation_order=3)
 
-filename_adhd = r"C:\Users\Asus\OneDrive\Desktop\NL2\FDAxEEG\Dataset\ADHD_Matrici_pyulear\ch"+canale+"_p"+banda
-mat = scipy.io.loadmat(filename_adhd)
-PSD_ADHD=mat['p'].T
-df_Channel=pd.DataFrame(data=PSD_ADHD)
-Channel=df_Channel.to_numpy(dtype=None, copy=False)
-Channel = np.nan_to_num(Channel)
-ADHD=skfda.FDataGrid(data_matrix=Channel)
-ADHD.interpolation=skfda.representation.interpolation.SplineInterpolation(interpolation_order=3)
+    #ADHD.plot()
 
-ADHD.plot()
-
-filename_control = r"C:\Users\Asus\OneDrive\Desktop\NL2\FDAxEEG\Dataset\Control_Matrici_pyulear\ch"+canale+"_p"+banda
-mat = scipy.io.loadmat(filename_control)
-
-PSD_control=mat['p'].T
+    filename_control = r"C:\Users\Asus\OneDrive\Desktop\NL2\FDAxEEG\Dataset\Control_Matrici_pyulear\ch"+canale+"_p"+banda
+    mat = scipy.io.loadmat(filename_control)
     
-df_Channel=pd.DataFrame(data=PSD_control)
-Channel=df_Channel.to_numpy(dtype=None, copy=False)
-Channel = np.nan_to_num(Channel)
-Control=skfda.FDataGrid(data_matrix=Channel)
-Control.interpolation=skfda.representation.interpolation.SplineInterpolation(interpolation_order=3)
+    PSD_control=mat['p'].T
+        
+    df_Channel=pd.DataFrame(data=PSD_control)
+    Channel=df_Channel.to_numpy(dtype=None, copy=False)
+    Channel = np.nan_to_num(Channel)
+    Control=skfda.FDataGrid(data_matrix=Channel)
+    Control.interpolation=skfda.representation.interpolation.SplineInterpolation(interpolation_order=3)
 
-Control.plot()
+    #Control.plot()
 
-Etichette=np.append(np.zeros(427),np.ones(555))
-Dati=np.concatenate((PSD_control,PSD_ADHD),axis=0)
+    Etichette=np.append(np.zeros(427),np.ones(555))
+    Dati=np.concatenate((PSD_control,PSD_ADHD),axis=0)
 
-df_Channel=pd.DataFrame(data=Dati)
-Channel=df_Channel.to_numpy(dtype=None, copy=False)
-Channel = np.nan_to_num(Channel)
-Misto=skfda.FDataGrid(data_matrix=Channel)
-Misto.interpolation=skfda.representation.interpolation.SplineInterpolation(interpolation_order=3)
+    df_Channel=pd.DataFrame(data=Dati)
+    Channel=df_Channel.to_numpy(dtype=None, copy=False)
+    Channel = np.nan_to_num(Channel)
+    Misto=skfda.FDataGrid(data_matrix=Channel)
+    Misto.interpolation=skfda.representation.interpolation.SplineInterpolation(interpolation_order=3)
 
-Misto.plot()
+    #Misto.plot()
 
 # Dati_train, Dati_test, Etichette_train, Etichette_test = train_test_split(Misto, Etichette, test_size=0.25,
 #                                                     stratify=Etichette, random_state=0)
@@ -101,7 +103,7 @@ Misto.plot()
 # score = knn.score(Dati_test, Etichette_test)
 # print(score)
 
-v_n, p_val = oneway_anova(Control,ADHD)
-
+    v_n, p_val = oneway_anova(Control,ADHD)
+    p.append(p_val)
 # clustering = AgglomerativeClustering().fit(Dati)
 # AgglomerativeClustering()
